33001d2a7039520abdd48a5eb400eb32441e6794
Jonathan Zhang <admin@cestlavie.moe>
2023-01-17 21:22:10 +0800

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -10,21 +10,6 @@ from WebStreamer.utils import get_hash, get_name
 from pyrogram.enums.parse_mode import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
-# Start Aria2 Config
-data = {
-    "jsonrpc": "2.0",
-    "method": "aria2.addUri",
-    "id": "1",
-    "params": [f"token:{Var.ARIA2_SECRET}",["url"]]
-}
-
-headers = {
-    'content-type': 'application/json',
-    'Authorization': f'Basic {Var.ARIA2_SECRET}'
-}
-
-rpc_url = Var.ARIA2_RPC_URL
-# End Aria2 Config
 
 @StreamBot.on_message(
     filters.private
@@ -41,13 +26,28 @@ rpc_url = Var.ARIA2_RPC_URL
     group=4,
 )
 async def media_receive_handler(_, m: Message):
+    # Start Aria2 Config
+    data = {
+        "jsonrpc": "2.0",
+        "method": "aria2.addUri",
+        "id": "1",
+        "params": [f"token:{Var.ARIA2_SECRET}",["url"]]
+    }
+
+    headers = {
+        'content-type': 'application/json',
+        'Authorization': f'Basic {Var.ARIA2_SECRET}'
+    }
+
+    rpc_url = Var.ARIA2_RPC_URL
+    # End Aria2 Config
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
     file_hash = get_hash(log_msg, Var.HASH_LENGTH)
     stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(m))}?hash={file_hash}"
     data['params'][1][0] = stream_link
     short_link = f"{Var.URL}{file_hash}{log_msg.id}"
     logger.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
-    response = requests.post(rpc_url, data=data, headers=headers)
+    response = requests.post(rpc_url, data=json.dumps(data), headers=headers)
     logger.info(response.text)
     try:
         await m.reply_text(
