613ba4dd80b09f872598bc830902ba5a809188b0
Jonathan Zhang <admin@cestlavie.moe>
2023-01-17 18:26:24 +0800

    Update stream.py

--- WebStreamer/bot/plugins/stream.py
+++ WebStreamer/bot/plugins/stream.py
@@ -1,7 +1,7 @@
 # This file is a part of TG-FileStreamBot
 # Coding : Jyothis Jayanth [@EverythingSuckz]
 
-import logging
+import logging,requests,json
 from pyrogram import filters, errors
 from WebStreamer.vars import Var
 from urllib.parse import quote_plus
@@ -10,6 +10,21 @@ from WebStreamer.utils import get_hash, get_name
 from pyrogram.enums.parse_mode import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton
 
+# Start Aria2 Config
+data = {
+    "jsonrpc": "2.0",
+    "method": "aria2.addUri",
+    "id": "1",
+    "params": [f"token:{ARIA2_SECRET}",["url"]]
+}
+
+headers = {
+    'content-type': 'application/json',
+    'Authorization': f'Basic {Var.ARIA2_SECRET}'
+}
+
+rpc_url = Var.ARIA2_RPC_URL
+# End Aria2 Config
 
 @StreamBot.on_message(
     filters.private
@@ -29,8 +44,11 @@ async def media_receive_handler(_, m: Message):
     log_msg = await m.forward(chat_id=Var.BIN_CHANNEL)
     file_hash = get_hash(log_msg, Var.HASH_LENGTH)
     stream_link = f"{Var.URL}{log_msg.id}/{quote_plus(get_name(m))}?hash={file_hash}"
+    data['params'][1][0] = stream_link
     short_link = f"{Var.URL}{file_hash}{log_msg.id}"
     logger.info(f"Generated link: {stream_link} for {m.from_user.first_name}")
+    response = requests.post(url, data=data, headers=headers)
+    logger.info(response.text)
     try:
         await m.reply_text(
             text="<code>{}</code>\n(<a href='{}'>shortened</a>)".format(
